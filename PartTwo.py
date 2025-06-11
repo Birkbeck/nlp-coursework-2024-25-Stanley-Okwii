ab60aa01396ec7747c66e31c486f0ff6f2e16390
StanleeO <stanlee.okwii@gmail.com>
2025-06-11 12:16:05 +0100

    Vectorize speeches and split dataset into train/test dataset with stratified sampling

--- PartTwo.py
+++ PartTwo.py
@@ -1,5 +1,8 @@
 from pathlib import Path
+
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
 
 def create_hansard_df():
@@ -23,8 +26,28 @@ def create_hansard_df():
 
     return df
 
-
 if __name__ == "__main__":
-    # a) Read hansard40000 data and subsets and renames dataframe
+    # (a) Read hansard40000 data and subsets and renames dataframe
     df = create_hansard_df()
-    print('df.shape \n', df.shape)
+    print(f"df.shape:  {df.shape} \n")
+
+
+    # (b) Vectorise the speeches with TfidfVectorizer
+    random_seed = 26
+    n_features = 3000
+    vectorizer = TfidfVectorizer(
+        max_features=n_features, stop_words="english"
+    )
+    X = vectorizer.fit_transform(df['speech'])
+    y = df['party']
+
+    # split dataset into train(80%) and test(20%)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y,
+        test_size=0.2,
+        stratify=y,
+        random_state=random_seed
+    )
+    print(f"Train dataset shape: {X_train.shape} \n")
+    print(f"Test dataset shape: {X_test.shape} \n")  
+
