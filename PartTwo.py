2eec57c4ee955f1393edf70a79327d6fde8babdb
StanleeO <stanlee.okwii@gmail.com>
2025-06-11 19:10:00 +0100

    Add custom tokenizer function and use it to evaluate model performance

--- PartTwo.py
+++ PartTwo.py
@@ -1,4 +1,6 @@
+import re
 from pathlib import Path
+from itertools import chain
 
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -6,11 +8,16 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, f1_score
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.util import ngrams
+from nltk.stem import PorterStemmer
 
 
 random_seed = 26
 n_features = 3000
-
+stop_words = stopwords.words('english')
+stemmer = PorterStemmer()
 
 def create_hansard_df():
     """
@@ -55,9 +62,24 @@ def report_evaluation_metrics(y_test, predictions):
     )  # Ignore division by zero warnings
 
 def train_and_evaluate_model(model, X_train, X_test, y_train, y_test):
+    """
+    """
     model.fit(X_train, y_train)
     model_predictions = model.predict(X_test)
     report_evaluation_metrics(y_test, model_predictions)
+
+def super_tokenizer(text):
+    """
+    """
+    text = re.sub(r'[^a-zA-Z\s]', '', text.lower()) # Ignore specials characters like punctuations
+    tokens = word_tokenize(text)
+
+    unigrams = [stemmer.stem(word) for word in tokens if (len(word) > 2 and word not in stop_words)] # Ignore 2 character words and stop words
+    bigrams = ['_'.join(gram) for gram in ngrams(unigrams, 2)]
+    trigrams = ['_'.join(gram) for gram in ngrams(unigrams, 3)]
+
+    result = list(chain(unigrams, bigrams, trigrams))
+    return result
 
 
 if __name__ == "__main__":
@@ -65,34 +87,54 @@ if __name__ == "__main__":
     df = create_hansard_df()
     print(f"df.shape:  {df.shape} \n")
 
+
     # (b) Vectorize the speeches with TfidfVectorizer
     vectorizer = TfidfVectorizer(
         max_features=n_features, stop_words="english"
     )
     X_train, X_test, y_train, y_test = vectorize_and_split_dataset(vectorizer, df)
 
+
     # (c) Train RandomForest and SVM with linear kernel classifiers
-    print("Training Random Forest Classifier...")
+    print("Fit Random Forest Classifier...")
     rf = RandomForestClassifier(n_estimators=300, random_state=random_seed)
     train_and_evaluate_model(rf, X_train, X_test, y_train, y_test)
 
     # SVM Classifier (Linear Kernel)
-    print("\nTraining SVM (Linear Kernel)...")
+    print("\nFit SVM (Linear Kernel)...")
     svm = SVC(kernel="linear", random_state=random_seed)
     train_and_evaluate_model(svm, X_train, X_test, y_train, y_test)
 
 
-    # (d) Tfidfvectorizer with unigrams
+    # (d) Classifiers using Tfidfvectorizer with unigrams
     ngram_vectorizer = TfidfVectorizer(
         max_features=n_features, stop_words="english", ngram_range=(1, 3)
     )
     X_train, X_test, y_train, y_test = vectorize_and_split_dataset(ngram_vectorizer, df)
-    print("Training Random Forest Classifier with ngram vectorizer...")
+    print("\nFit Random Forest Classifier that uses ngram vectorizer...")
     rf = RandomForestClassifier(n_estimators=300, random_state=random_seed)
     train_and_evaluate_model(rf, X_train, X_test, y_train, y_test)
 
     # SVM Classifier (Linear Kernel)
-    print("\nTraining SVM (Linear Kernel) with ngram vectorizer...")
+    print("\nFit SVM (Linear Kernel) that uses ngram vectorizer...")
     svm = SVC(kernel="linear", random_state=random_seed)
     train_and_evaluate_model(svm, X_train, X_test, y_train, y_test)
 
+
+    # (e) Classifiers using Tfidfvectorizer with custom tokenizer
+    custom_vectorizer = TfidfVectorizer(
+        tokenizer=super_tokenizer,
+        max_features=n_features,
+        ngram_range=(1, 3),
+        sublinear_tf=True,
+        token_pattern=None,  # disable default regex warning
+    )
+    X_train, X_test, y_train, y_test = vectorize_and_split_dataset(custom_vectorizer, df)
+    print("\nFit Random Forest Classifier with super tokenizer...")
+    rf = RandomForestClassifier(n_estimators=300, random_state=random_seed)
+    train_and_evaluate_model(rf, X_train, X_test, y_train, y_test)
+
+    # SVM Classifier (Linear Kernel)
+    print("\nFit SVM (Linear Kernel) with super tokenizer...")
+    svm = SVC(kernel="linear", random_state=random_seed)
+    train_and_evaluate_model(svm, X_train, X_test, y_train, y_test)
